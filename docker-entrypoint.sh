45c62f689050b55f600ac69c26bd888541792ce4
Maarten Vercruysse <67873169+MV-GH@users.noreply.github.com>
2023-08-13 13:27:19 +0200

    Update docker-entrypoint.sh attempt 3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,4 @@ echo "redocly version: $(redocly --version)"
 
 output=$(redocly $1)
 
-echo "output<<$EOF" >> $GITHUB_OUTPUT
-echo "$output" >> $GITHUB_OUTPUT
-echo "$EOF" >> $GITHUB_OUTPUT
+echo "output<<EOF"$'\n'"$output"$'\n'EOF >> $GITHUB_OUTPUT
